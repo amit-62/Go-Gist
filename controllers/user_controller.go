63e0afeda45398f740d0112187e2ebc4a086576e
Amit Kumar <kramit6662@gmail.com>
2023-10-18 23:38:40 +0530

    added controller, middelware and utils
    Signed-off-by: Amit Kumar <kramit6662@gmail.com>

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,1 +1,37 @@
-package controlller
+package controllers
+
+import (
+	"net/http"
+
+	"gihub.com/amit/Go-Gist/models"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type UserController struct {
+	DB *gorm.DB
+}
+
+func NewUserController(DB *gorm.DB) UserController {
+	return UserController{
+		DB: DB,
+	}
+}
+
+func (uc *UserController) GetMe(ctx *gin.Context) {
+	currentUser := ctx.MustGet("currentUser").(models.User)
+
+	userResponse := &models.UserResponse{
+		Username:     currentUser.Username,
+		FirstName:    currentUser.FirstName,
+		LastName:     currentUser.LastName,
+		Email:        currentUser.Email,
+		Role:         currentUser.Role,
+		Provider:     currentUser.Provider,
+		UserMetadata: currentUser.UserMetadata,
+		CreatedAt:    currentUser.CreatedAt,
+		UpdatedAt:    currentUser.UpdatedAt,
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": gin.H{"user": userResponse}})
+}
